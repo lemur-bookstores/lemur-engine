ebab640e2d1996ef6cb81b53acf660775085209d
Elkin Portocarrero <elkincp5@gmail.com>
2025-07-20 16:56:16 -0500

    feat(config-events): extender interfaz ConfigChangeListener para soportar eventos genéricos

--- src/core/config/interfaces/ConfigChangeListener.ts
+++ src/core/config/interfaces/ConfigChangeListener.ts
@@ -1,4 +1,5 @@
 import { KernelConfig } from '../types';
+import { ConfigChangeEvent } from '../events/ConfigEventManager';
 
 export interface ConfigChangeListener {
     /**
@@ -6,5 +7,11 @@ export interface ConfigChangeListener {
      * @param oldConfig Configuración anterior
      * @param newConfig Nueva configuración
      */
-    onConfigChange(oldConfig: KernelConfig, newConfig: KernelConfig): void;
+    onConfigChange?(oldConfig: KernelConfig, newConfig: KernelConfig): void;
+
+    /**
+     * Método llamado para cualquier evento de configuración
+     * @param event Evento de configuración
+     */
+    onEvent?(event: ConfigChangeEvent): void;
 }
